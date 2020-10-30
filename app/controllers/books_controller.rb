b8a021adb31bb2f7dfbe03dfc9908debb54636b1
awoka333 <na23okzw.htre@gmail.com>
2020-10-30 13:42:19 +0000

    modified 80%

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,54 @@
 class BooksController < ApplicationController
+
   def start
   end
 
   def index
+    @books = Book.all
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to '/books/#{@book.id}'
+      else
+        flash.now[:alart] = "Error occered!!"
+        render :index
+      end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully modified."
+      redirect_to '/books/#{@book.id}'
+    else
+      flash.now[:alart] = "Error occered!!"
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
 end
