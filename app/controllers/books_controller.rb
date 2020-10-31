c24f5a5e5020334a3da001f984f0c37f81241eef
awoka333 <na23okzw.htre@gmail.com>
2020-10-31 07:23:28 +0000

    completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,19 +4,17 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
-  end
-
-  def new
   end
 
   def create
     @book = Book.new(book_params)
       if @book.save
         flash[:notice] = "Book was successfully created."
-        redirect_to '/books/#{@book.id}'
+        redirect_to book_path(@book)
       else
-        flash.now[:alart] = "Error occered!!"
+        @books = Book.all
         render :index
       end
   end
@@ -30,20 +28,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully modified."
-      redirect_to '/books/#{@book.id}'
+      redirect_to book_path(@book)
     else
-      flash.now[:alart] = "Error occered!!"
-      render :edit
+      render "edit"
     end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully deleted."
+    redirect_to books_path
   end
 
   private
